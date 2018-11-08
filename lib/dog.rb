6b72f9dc9415c7f133b4587dc47aa759bf66b2ce
hugo valois <hvgaming@yahoo.com>
2018-11-08 02:23:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,4 @@
 class Dog < ActiveRecord::Base
   
   
-  def self.save
-  end
-  
-  def self.create
-    
-  end
-  
- def self.update
-  end
-  
-  def self.find_or create_by
-  end
-   
-   def 
-     self.find_by_name
-  end
-  
-  def self.find_by_id
-    
-  end
-  
-  
 end
